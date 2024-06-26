07ba5bc7657236718e6ec1bf46df8fe5cf0e8cad
Ali-2544 <codewithpassion4@gmail.com>
2024-06-26 05:17:04 -0700

    new repo created

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "@/app/globals.css"
+
+function index() {
+  return (
+    <div>
+      <h1 className='pt-10 text-center'>Welcome to GenRoar Development team :)</h1>
+    </div>
+  )
+}
+
+export default index
