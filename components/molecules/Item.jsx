b9f7106806ab0c1beef2aadf2821c8baaf34cbcc
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-07-05 03:46:21 -0700

    saqib

--- components/molecules/Item.jsx
+++ components/molecules/Item.jsx
@@ -5,10 +5,10 @@ import { PiBasket } from "react-icons/pi";
 function Item() {
   return (
     <div>
-        <div className='flex gap-[10px] hover:text-[#ff6a28] border-[1px] border-[#dbdbdb] rounded-[50px] py-[8px] px-[10px]'>
-        <PiBasket className='' />
-            <AnchorTag variant="productName">2 Items(s)</AnchorTag>
-        </div>
+      <div className='flex gap-[10px] hover:text-[#ff6a28] border-[1px] border-[#dbdbdb] rounded-[50px] py-[8px] px-[10px] hover:cursor-pointer	'>
+        <PiBasket />
+        <AnchorTag variant="productName">2 Items(s)</AnchorTag>
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
 import Heading from "@/components/atoms/Heading";
 import AnchorTag from "@/components/atoms/anchorTag";
 import Headermid from "@/components/molecules/Headermid";
-import Home from "./home";
+// import Home from "@/components/home"; // Commented out because it causes an error
 import AboutData from "@/components/organisms/AboutData";
 import Counter from "@/components/organisms/Counter";
 import Container from "@/components/atoms/Container";
-import Progress from "@/components/organisms/Progress"
+import Progress from "@/components/organisms/Progress";
 import SearchHeader from "@/components/molecules/SearchHeader";
 import Headermain from "@/components/organisms/Headermain";
 import Ried from "@/components/molecules/Ried";
@@ -15,19 +15,16 @@ import Footer from "@/components/organisms/Footer";
 
 function Index() {
   return (
-    <div className=' ' >
-      
-      
-      <Headermain/>
+    <div className="">
+      <Headermain />
       {/* <Home /> */}
-      <AboutData/>
-      <Ried/>
-      <Counter/>
-      <Progress/>
-      <Footer/>
-    
+      <AboutData />
+      <Ried />
+      <Counter />
+      <Progress />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Index;
