57813bfadb9fee3431abbbe37e450509089c986c
Aresha Sohail <138809867+AreshaSohail1@users.noreply.github.com>
2024-07-05 02:15:57 -0700

    Aresha

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 "use client"
 
-import Heading from "@/components/atoms/Heading";
-import AnchorTag from "@/components/atoms/anchorTag";
-import Headermid from "@/components/molecules/Headermid";
-import Home from "./home";
+
 import AboutData from "@/components/organisms/AboutData";
 import Counter from "@/components/organisms/Counter";
-import Container from "@/components/atoms/Container";
 import Progress from "@/components/organisms/Progress"
-import SearchHeader from "@/components/molecules/SearchHeader";
 import Headermain from "@/components/organisms/Headermain";
 import Ried from "@/components/molecules/Ried";
+
 import Footer from "@/components/organisms/Footer";
 
 function Index() {
@@ -19,7 +15,6 @@ function Index() {
       
       
       <Headermain/>
-      {/* <Home /> */}
       <AboutData/>
       <Ried/>
       <Counter/>
