0c5a0e0f3ab6956bd8498e8b0b1f7ece5095485a
Ayesha Mukhtar <codewithpassion5@gmail.com>
2024-07-05 16:03:29 +0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
-"use client";
+"use client"
 
-<<<<<<< HEAD
-import Heading from "@/components/atoms/Heading";
-import AnchorTag from "@/components/atoms/anchorTag";
-import Headermid from "@/components/molecules/Headermid";
-// import Home from "@/components/home"; // Commented out because it causes an error
-import AboutData from "@/components/organisms/AboutData";
-import Counter from "@/components/organisms/Counter";
-import Container from "@/components/atoms/Container";
-import Progress from "@/components/organisms/Progress";
-import SearchHeader from "@/components/molecules/SearchHeader";
-=======
 
 import AboutData from "@/components/organisms/AboutData";
 import Counter from "@/components/organisms/Counter";
 import Progress from "@/components/organisms/Progress"
->>>>>>> 57813bfadb9fee3431abbbe37e450509089c986c
 import Headermain from "@/components/organisms/Headermain";
 import Ried from "@/components/molecules/Ried";
 
@@ -23,16 +11,6 @@ import Footer from "@/components/organisms/Footer";
 
 function Index() {
   return (
-<<<<<<< HEAD
-    <div className="">
-      <Headermain />
-      {/* <Home /> */}
-      <AboutData />
-      <Ried />
-      <Counter />
-      <Progress />
-      <Footer />
-=======
     <div className=' ' >
       
       
@@ -43,9 +21,8 @@ function Index() {
       <Progress/>
       <Footer/>
     
->>>>>>> 57813bfadb9fee3431abbbe37e450509089c986c
     </div>
-  );
+  )
 }
 
 export default Index;
