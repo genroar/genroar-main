1620f7ade539f049c4baac8974603ab2fbd2a924
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-07-08 03:54:47 -0700

    saqib

--- components/molecules/Headermid.jsx
+++ components/molecules/Headermid.jsx
@@ -12,18 +12,18 @@ function Headermid() {
         <li><AnchorTag variant='Header' href='/'>Shop</AnchorTag></li>
         <li><AnchorTag variant='Header' href='/'>Pages<IoIosArrowDown /></AnchorTag>
           <ul className='dropdown border absolute bg-white opacity-5 z-[9999] invisible py-[10px] pr-[80px] pl-[30px]'>
-            <li><AnchorTag variant='Headerh' href='/'> About Us</AnchorTag></li>
+            <li><AnchorTag variant='Headerh' href='/about'> About Us</AnchorTag></li>
             <li><AnchorTag variant='Headerh' href='/'>Services</AnchorTag></li>
-            <li><AnchorTag variant='Headerh' href='/'>Login</AnchorTag></li>
+            <li><AnchorTag variant='Headerh' href='/login'>Login</AnchorTag></li>
             <li><AnchorTag variant='Headerh' href='/'>My Account</AnchorTag></li>
-            <li><AnchorTag variant='Headerh' href='/'>Error 404</AnchorTag></li>
+            <li><AnchorTag variant='Headerh' href='/error'>Error 404</AnchorTag></li>
             <li><AnchorTag variant='Headerh' href='/'>Compare</AnchorTag></li>
-            <li><AnchorTag variant='Headerh' href='/'>Privacy Policy</AnchorTag></li>
+            <li><AnchorTag variant='Headerh' href='/privacy'>Privacy Policy</AnchorTag></li>
             <li><AnchorTag variant='Headerh' href='/'>Coming Soon</AnchorTag></li>
           </ul>
         </li>
         <li><AnchorTag variant='Header' href='/about'>About US</AnchorTag></li>
-        <li><AnchorTag variant='Header' href='/'>Contact Us</AnchorTag></li>
+        <li><AnchorTag variant='Header' href='/contact'>Contact Us</AnchorTag></li>
       </div>
       <div className=''>
         <Paragraph>Call Free Support: <AnchorTag variant='success'>01234567890</AnchorTag></Paragraph>
