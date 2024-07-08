2d65662fd950c0f491a85e5cf0b8eef04892fa9c
Ayesha Mukhtar <codewithpassion5@gmail.com>
2024-07-08 12:55:19 +0500

    ayesha

--- components/molecules/Headermid.jsx
+++ components/molecules/Headermid.jsx
@@ -9,7 +9,18 @@ function Headermid() {
         <div className='flex  list-none items-center gap-[60px]  '>
             <li><AnchorTag variant='Header' href='/'>Home</AnchorTag></li>
             <li><AnchorTag variant='Header' href='/'>Shop</AnchorTag></li>
-            <li><AnchorTag variant='Header' href='/'>Pages</AnchorTag></li>
+            <li><AnchorTag variant='Header' href='/'>Pages</AnchorTag>
+             <ul className='dropdown border absolute opacity-5 z-0 invisible pr-[100px] pl-[30px]  '>
+              <li><AnchorTag variant='Headerh' href='/'> About Us</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>Services</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>Login</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>My Account</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>Error 404</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>Compare</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>Privacy Policy</AnchorTag></li>
+              <li><AnchorTag variant='Headerh' href='/'>Coming Soon</AnchorTag></li>
+             </ul>
+            </li>
             <li><AnchorTag variant='Header' href='/about'>About US</AnchorTag></li>
             <li><AnchorTag variant='Header' href='/'>Contact Us</AnchorTag></li>
         </div>
