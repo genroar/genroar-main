52a83eff463f381392e7252c71088251a8dd0594
SAQIB <154792058+saqib48@users.noreply.github.com>
2024-07-13 03:47:41 -0700

    saqib

--- components/organisms/Shop1.jsx
+++ components/organisms/Shop1.jsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
-import { Row, Col, Select, Pagination } from 'antd';
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Pagination } from 'antd';
 import { BsGrid3X3GapFill } from "react-icons/bs";
 import { TfiLayoutGrid4Alt } from "react-icons/tfi";
-import "../../app/globals.css"
 import { FaBars } from "react-icons/fa";
+import "../../app/globals.css";
 import ShopProduct from '../molecules/ShopProduct';
 import products from './Products';
-import 'antd/dist/reset.css';
 import Heading from '../atoms/Heading';
-
-const { Option } = Select;
 
 const Shop1 = () => {
     const [layout, setLayout] = useState('grid3');
     const [columns, setColumns] = useState(3);
     const [pageSize, setPageSize] = useState(10);
     const [currentPage, setCurrentPage] = useState(1);
+    const [hydrated, setHydrated] = useState(false);
+
+    useEffect(() => {
+        setHydrated(true);
+    }, []);
+
+    if (!hydrated) {
+        return null; // or a loading indicator
+    }
 
     const handleGridClick = (value) => {
         setLayout(value);
@@ -30,6 +38,7 @@ const Shop1 = () => {
                 setColumns(5);
                 break;
             case 'list':
+                setColumns(1);
                 break;
             default:
                 break;
@@ -55,7 +64,7 @@ const Shop1 = () => {
                     <a href="#" onClick={() => handleGridClick('grid5')}><TfiLayoutGrid4Alt className='w-[20px] h-[20px] text-gray-200 hover:text-gray-600 duration-300' /></a>
                     <a href="#" onClick={() => handleGridClick('list')}><FaBars className='w-[20px] h-[20px] text-gray-200 hover:text-gray-600 duration-300' /></a>
                 </div>
-                <select className='mid border py-[8px] text-[#747474] px-[8px] outline-none rounded-[5px]  text-[15px] font-medium ' name="" id="">
+                <select className='mid border py-[8px] text-[#747474] px-[8px] outline-none rounded-[5px] text-[15px] font-medium'>
                     <option value="">Sort by average rating</option>
                     <option value="">Sort by popularity</option>
                     <option value="">Sort by newness</option>
@@ -63,7 +72,7 @@ const Shop1 = () => {
                     <option value="">Sort by price: high to low</option>
                     <option value="">Product Name: Z</option>
                 </select>
-                <Heading level='7' >Showing 1–9 of 21 results</Heading>
+                <Heading level='7'>Showing 1–9 of 21 results</Heading>
             </div>
             <Row gutter={[16, 16]}>
                 {paginatedProducts.map((product, index) => (
